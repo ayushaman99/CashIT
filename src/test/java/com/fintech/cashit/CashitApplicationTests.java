7e420ca232075772745c8739e327bbd86bfffe50
ayushaman99 <ayushaman9923@gmail.com>
2026-08-26 18:09:36 +0530

    setting up the databse

---
+++ src/test/java/com/fintech/cashit/CashitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.cashit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
