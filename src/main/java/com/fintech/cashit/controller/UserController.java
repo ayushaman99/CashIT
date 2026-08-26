2822404bf3937ee48151bb998543e2afafef7018
ayushaman99 <ayushaman9923@gmail.com>
2026-08-26 19:43:07 +0530

    created and tested api endpoints

--- src/main/java/com/fintech/cashit/controller/UserController.java
+++ src/main/java/com/fintech/cashit/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.fintech.cashit.controller;
 
+import com.fintech.cashit.entity.User;
 import com.fintech.cashit.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +18,10 @@ public class UserController {
     @GetMapping("/hello")
     public String hello(){
         return "its working";
+    }
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user)
+    {
+        return userService.saveUser(user);
     }
 }
