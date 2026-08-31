efc50b47b3613e68e957647b981bd06fcc5c7de0
ayushaman99 <ayushaman9923@gmail.com>
2026-08-31 20:04:53 +0530

    basic validation

--- src/main/java/com/fintech/cashit/DTO/OrderRequestDTO.java
+++ src/main/java/com/fintech/cashit/DTO/OrderRequestDTO.java
@@ -1,4 +1,7 @@
 package com.fintech.cashit.DTO;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 import java.math.BigDecimal;
 
 public class OrderRequestDTO {
@@ -17,7 +20,8 @@ public class OrderRequestDTO {
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
-
+    @Positive
+    @NotNull
     private BigDecimal amount;
     private String description;
 }
