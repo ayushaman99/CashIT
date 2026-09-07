1e345d60adc875b5b91c5714e5cd41576c1988b8
ayushaman99 <ayushaman9923@gmail.com>
2026-09-07 16:47:22 +0530

    implementing idempotency

---
+++ src/main/java/com/fintech/cashit/entity/Idempotency.java
@@ -0,0 +1,51 @@
+package com.fintech.cashit.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "idempotency_keys")
+public class Idempotency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String idempotencyKey;
+
+    @OneToOne
+    private Payment payment;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getIdempotencyKey() {
+        return idempotencyKey;
+    }
+
+    public void setIdempotencyKey(String idempotencyKey) {
+        this.idempotencyKey = idempotencyKey;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
