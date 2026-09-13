389a3cc36c2459980a7fe75e01d77655cb3be319
ayushaman99 <ayushaman9923@gmail.com>
2026-09-13 09:51:44 +0530

    payment linl service created

---
+++ src/main/java/com/fintech/cashit/service/PaymentLinkService.java
@@ -0,0 +1,46 @@
+package com.fintech.cashit.service;
+
+import com.fintech.cashit.entity.Order;
+import com.fintech.cashit.entity.PaymentLink;
+import com.fintech.cashit.repository.OrderRepository;
+import com.fintech.cashit.repository.PaymentLinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+public class PaymentLinkService {
+
+    @Autowired
+    private PaymentLinkRepository paymentLinkRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public PaymentLink createPaymentLink(
+            Long orderId,
+            Authentication authentication) {
+
+        var user = authentication.getPrincipal();
+
+        Order order = orderRepository
+                .findById(orderId)
+                .orElseThrow(() ->
+                        new RuntimeException("Order not found"));
+
+        PaymentLink link = new PaymentLink();
+
+        link.setLinkReference(
+                UUID.randomUUID().toString()
+        );
+
+        link.setOrder(order);
+        link.setActive(true);
+        link.setCreatedAt(LocalDateTime.now());
+
+        return paymentLinkRepository.save(link);
+    }
+}
