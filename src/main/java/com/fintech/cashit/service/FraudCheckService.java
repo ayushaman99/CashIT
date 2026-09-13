4b8d6655e8aa5342f7b5d1d9aae1f53eaf8b51e1
ayushaman99 <ayushaman9923@gmail.com>
2026-09-13 10:06:53 +0530

    implemented fraud detections

--- src/main/java/com/fintech/cashit/service/FraudCheckService.java
+++ src/main/java/com/fintech/cashit/service/FraudCheckService.java
@@ -1,19 +1,42 @@
 package com.fintech.cashit.service;
 
 import com.fintech.cashit.entity.Payment;
+import com.fintech.cashit.repository.PaymentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 public class FraudCheckService {
 
+    @Autowired
+    private PaymentRepository paymentRepository;
+
     public boolean isFraudulent(Payment payment) {
+
 
         if (payment.getAmount().compareTo(new BigDecimal("100000")) > 0) {
             return true;
         }
+
+
         if (payment.getAmount().compareTo(new BigDecimal("1")) < 0) {
+            return true;
+        }
+
+
+        LocalDateTime oneMinuteAgo =
+                LocalDateTime.now().minusMinutes(1);
+
+        long recentPayments =
+                paymentRepository.countByOrder_UserAndCreatedAtAfter(
+                        payment.getOrder().getUser(),
+                        oneMinuteAgo
+                );
+
+        if (recentPayments >= 5) {
             return true;
         }
 
