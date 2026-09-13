03930d84bd5d0cd6d7bddf5a80a2883d485056ed
ayushaman99 <ayushaman9923@gmail.com>
2026-09-13 09:58:02 +0530

    fraud checking for one lakh rps payment

---
+++ src/main/java/com/fintech/cashit/service/FraudCheckService.java
@@ -0,0 +1,19 @@
+package com.fintech.cashit.service;
+
+import com.fintech.cashit.entity.Payment;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class FraudCheckService {
+
+    public boolean isFraudulent(Payment payment) {
+
+        if (payment.getAmount().compareTo(new BigDecimal("100000")) > 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
